29e5e6e5ebc2f42c85d0c440f81304af70792e0a
Alexander List <atlex2@gmail.com>
2014-01-19 21:45:20 -0500

    got setup for headband

--- ET Reader/ETViewController.h
+++ ET Reader/ETViewController.h
@@ -10,6 +10,9 @@
 #import "ETBTLEManager.h"
 #import "ETDataManager.h"
 #import "GraphView.h"
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
+
 
 @class ETViewController;
 
@@ -26,11 +29,21 @@
 @property (nonatomic, assign)BOOL canConnect;
 @property (nonatomic, weak) id<ETViewControllerDelegate> delegate;
 
+
+@property (nonatomic, strong) AVAudioPlayer * triggerAudio;
+@property (nonatomic, strong) AVAudioSession * triggerAudioSession;
+
+@property (nonatomic, assign) double triggerThreshold;
+
 @property (strong, nonatomic) ETBTLEManager* btleManager;
 @property (strong, nonatomic) ETDataManager * dataManager;
 
+@property (nonatomic, assign)BOOL enableTriggers;
+
 @property (weak, nonatomic) IBOutlet UITextView *outputView;
 @property (weak, nonatomic) IBOutlet UISwitch *connectedSwitch;
+@property (weak, nonatomic) IBOutlet UISlider *triggerThresholdSlider;
+@property (weak, nonatomic) IBOutlet UILabel *triggerThresholdLabel;
 
 @property (nonatomic, strong) GraphView * graphView;
 
@@ -41,9 +54,14 @@
 - (IBAction)versionRequestButtonPressed:(id)sender;
 - (IBAction)flagButtonPressed:(id)sender;
 - (IBAction)exportButtonPressed:(id)sender;
+- (IBAction)triggerThresholdSliderChanged:(id)sender;
+
+
+-(void) updateTriggerThresholdWithValue:(double)threshold;
 
 -(void) addDataStringToView:(NSString*)dataString;
 
+-(BOOL) setupAudioSession;
 
 -(void)_updateRawDataDisplay;
 -(void)_updateGraphView;
--- ET Reader/ETViewController.m
+++ ET Reader/ETViewController.m
@@ -36,19 +36,60 @@
     if (_dataManager == nil){
         _dataManager = [[ETDataManager alloc] init];
         [_dataManager setAllowFlags:TRUE];
-        [_dataManager setInterestingIndexes:[NSSet setWithArray:@[@(1),@(2),@(3)]]];
+        [_dataManager setInterestingIndexes:[NSSet setWithArray:@[@(1),@(2),@(3),@(4),@(5)]]];
         [_dataManager setDelegate:self];
-        [_dataManager setCsvColumnCount:6];
+        [_dataManager setCsvColumnCount:5];
     }
     return _dataManager;
 }
 
 
+-(BOOL) setupAudioSession{
+    
+    //activate
+    NSError *activationError = nil;
+    BOOL success = [self.triggerAudioSession setActive: YES error: &activationError];
+    if (!success) { /* handle the error in activationError */
+        EXOLog(@"Session activate error: %@", activationError);
+        return FALSE;
+    }
+    
+    //set category
+    NSError *setCategoryError = nil;
+    success = [self.triggerAudioSession
+               setCategory: AVAudioSessionCategoryPlayback
+               withOptions:AVAudioSessionCategoryOptionMixWithOthers error: &setCategoryError];
+    
+    if (!success) { /* handle the error in setCategoryError */
+        EXOLog(@"Session set category error: %@", activationError);
+        return FALSE;
+    }
+
+    return TRUE;
+}
+-(AVAudioSession*) triggerAudioSession; {
+    if (_triggerAudioSession == nil){
+        _triggerAudioSession = [AVAudioSession sharedInstance];
+        
+    }
+    return _triggerAudioSession;
+}
+
+
+
+-(void) updateTriggerThresholdWithValue:(double)threshold{
+    self.triggerThreshold = threshold; //default threshold
+    
+    [self.triggerThresholdLabel setText:[NSString stringWithFormat:NSLocalizedString(@"threshold: %f", @"threshold string"), self.triggerThreshold]];
+    self.triggerThresholdSlider.value = self.triggerThreshold/100.0;
+    
+}
+
 -(GraphView*) graphView{
     if (_graphView == nil){
         _graphView = [[GraphView alloc] initWithFrame:CGRectMake(180, 70, 375, 240)];
 
-        [_graphView setManualY:1024];//this is the 10bit dac from the arduino
+//        [_graphView setManualY:1024];//this is the 10bit dac from the arduino
 //        [self.view addSubview:_graphView];
     }
     
@@ -61,6 +102,10 @@
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self setupAudioSession];
+    
+    [self updateTriggerThresholdWithValue:1];
+    self.enableTriggers = TRUE;
 }
 
 - (void)didReceiveMemoryWarning
@@ -85,7 +130,7 @@
 } 
 
 - (IBAction)versionRequestButtonPressed:(id)sender {
-    [self.btleManager sendDataToPeripheral:[@"version;" dataUsingEncoding:NSUTF8StringEncoding ]];
+    [self.btleManager sendDataToPeripheral:[@"flash;" dataUsingEncoding:NSUTF8StringEncoding ]];
 }
 
 - (IBAction)flagButtonPressed:(id)sender {
@@ -108,6 +153,10 @@
     UIActivityViewController * activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[resultsDateString,fileUrl] applicationActivities:nil];
     [self presentViewController:activityVC animated:TRUE completion:nil];
 
+}
+
+- (IBAction)triggerThresholdSliderChanged:(id)sender {
+    [self updateTriggerThresholdWithValue: [self.triggerThresholdSlider value]*100];
 }
 - (IBAction)makeSessionActiveButtonPressed:(id)sender {
     [self.btleManager sendDataToPeripheral:[@"startSession;" dataUsingEncoding:NSUTF8StringEncoding ]];
@@ -135,7 +184,15 @@
 }
 
 -(void) _updateGraphView{
-    NSArray * series = [[self.dataManager indexedDataArrays] objectForKey:@(2)];//this is series 2, for pressure
+    //see https://github.com/alist/Arduino-Brain-Library
+    //0 state
+    //1 data millis
+    //2 quality
+    //3 for readMeditation
+    //4 for attention
+    //5 low beta higher when you're alert and focused
+    //6 low gamma multi-sensory processing
+    NSArray * series = [[self.dataManager indexedDataArrays] objectForKey:@(4)];
     //each point is spaced equally if in active mode
     
     NSInteger displayPointCount = MIN(1000, series.count);
@@ -148,13 +205,63 @@
     [self.graphView setArray:floatedArray];
 }
 
+
+-(void)_checkDataForTriggers{
+    
+    NSNumber * triggerSeries = @(3);//4=attention
+    
+    NSArray * series = [[self.dataManager indexedDataArrays] objectForKey:triggerSeries];
+    
+    NSInteger displayPointCount = MIN(2, series.count);
+    
+    BOOL doesTrigger = FALSE;
+    for (NSString* item in [series subarrayWithRange:NSMakeRange(series.count - displayPointCount, displayPointCount)]){
+//        EXOLog(@"Item: %i", [item integerValue]);
+        if ([item integerValue] >= self.triggerThreshold){
+            doesTrigger = TRUE;
+        }
+    }
+
+    
+    if (doesTrigger == TRUE){
+        [self performSelector:@selector(_playTriggerSound) withDebounceDuration:.5];
+    }
+}
+
+-(void)_playTriggerSound{
+    
+//    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:[NSDateFormatter localizedStringFromDate:[NSDate date] dateStyle:NSDateFormatterNoStyle timeStyle:NSDateFormatterShortStyle]];
+    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:@"ow"];
+
+    AVSpeechSynthesizer *synth = [[AVSpeechSynthesizer alloc] init];
+    
+    [synth speakUtterance:utterance];
+
+    
+    
+//    self.triggerAudio = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"Narrative-Jibberish-goodpoint" ofType:@"wav" inDirectory:@"/"]] error:nil];
+//	self.triggerAudio.volume = .7;
+//	self.triggerAudio.numberOfLoops = 1;
+//	int64_t delayInSeconds = 0.0;
+//	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+//	dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+//		[self.triggerAudio play];
+//	});
+
+}
+
 //ETDataManagerDelegate
 -(void)ETDataManagerDelegateDidUpdateData:(ETDataManager*)dataManager{
     [self performSelector:@selector(_updateRawDataDisplay) withDebounceDuration:.15];
     
     [self performSelector:@selector(_updateGraphView) withDebounceDuration:.15];
 
+    if (self.enableTriggers){
+        [self performSelector:@selector(_checkDataForTriggers) withDebounceDuration:.15];
+    }
 }
+
+
 
 //UIActivityItemSource //maybe not even needed
 // called to determine data type. only the class of the return type is consulted. it should match what -itemForActivityType: returns later
